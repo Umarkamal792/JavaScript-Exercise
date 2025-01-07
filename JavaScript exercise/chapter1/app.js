57bec905166c3207171a2fb15689bed6e0ee5a47
Umarkamal792 <132712455+Umarkamal792@users.noreply.github.com>
2025-01-07 06:08:48 +0500

    Add files via upload

---
+++ JavaScript exercise/chapter1/app.js
@@ -0,0 +1,16 @@
+ // ===========Question No: 1================
+
+ alert("Question NO:1 \nWelcome")
+
+ // ===========Question No: 2================
+
+ alert("Question NO:2 \nError! Please enter a valid password")
+
+ // ===========Question No: 3================
+
+ alert("Question NO:3 \nWelcome to JS land.... \nHappy Coding!")
+
+ // ===========Question No: 4================
+
+ alert("Question NO:4 \nWelcome to JS land....")
+ alert("Happy Coding!")
