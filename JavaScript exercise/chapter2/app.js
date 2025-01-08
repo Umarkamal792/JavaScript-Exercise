e20aada9cdb45975948172582f5d3288093a60f3
Umarkamal792 <132712455+Umarkamal792@users.noreply.github.com>
2025-01-08 22:50:58 +0500

    Add files via upload

---
+++ JavaScript exercise/chapter2/app.js
@@ -0,0 +1,44 @@
+// ==================Question NO: 1===========
+
+var username;
+
+// ==================Question NO: 2===========
+
+var myname = "Umar Kamal";
+
+// ==================Question NO: 3===========
+
+var msg = "Hello World";
+alert(msg)
+
+// ==================Question NO: 4===========
+
+var name = "Umar";
+var age = "21";
+var edu = "Software Developer";
+alert(name)
+alert(age)
+alert(edu)
+
+// ==================Question NO: 5===========
+
+alert("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+// ==================Question NO: 6===========
+
+var email = "umarkamal792@gmail.com"
+alert("My email address is " + email)
+
+// ==================Question NO: 7===========
+
+var book = "smart way to learn javascript"
+alert("I am trying to learn from the book " + book)
+
+// ==================Question NO: 8===========
+
+document.write("Yah! I can write HTML content by javascript")
+
+// ==================Question NO: 9===========
+
+var variable = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(variable)
