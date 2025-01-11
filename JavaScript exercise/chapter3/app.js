3b37265a0c229420180fd7c0daf64ab24761ee0d
Umarkamal792 <132712455+Umarkamal792@users.noreply.github.com>
2025-01-11 18:26:00 +0500

    Add files via upload

---
+++ JavaScript exercise/chapter3/app.js
@@ -0,0 +1,22 @@
+ //    ===============Question No:1==============
+
+ var age = 21
+ alert("i am " + age + " years old")
+
+ //    ===============Question No:2==============
+
+ var num = 2
+ alert("you have visited this site " + num + " times")
+
+ //    ===============Question No:3==============
+
+ var date = 2003
+ document.write("My birth year is " + date)
+ document.write("<br>" + "Data type of my birth year is " + typeof (date))
+
+//    ===============Question No:4==============
+
+ var visName = "John Doe"
+ var pro = "T-shirt(s)"
+ var ord = 4
+ document.write("<br><br>" + "<b>" + visName + "</b>" + " ordered " + "<b>" + ord + " " + pro + "</b>" + " on XYZ Clothing store")
