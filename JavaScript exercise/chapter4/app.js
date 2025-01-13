0d9225fe41c6d2351219e11abc948749b69829b5
Umarkamal792 <132712455+Umarkamal792@users.noreply.github.com>
2025-01-13 21:10:17 +0500

    Add files via upload

---
+++ JavaScript exercise/chapter4/app.js
@@ -0,0 +1,25 @@
+// ===========Question No: 1================
+
+var b, c, d;
+
+// ===========Question No: 2================
+
+var name;
+var $name;
+var name2;
+var _name;
+var name2nd;
+// var !name;
+// var 2name;
+// var var;
+// var first name;
+// var Sec%;
+
+// ===========Question No: 3================
+
+document.write("<p>" + "<h1>" + "Rules for naming JS variables" + "</h1>" + "</p>")
+document.write("<p>" + "Variable names can only contain $, _, letter and number" + "</p>")
+document.write("<p>" + "Variable must begin with a $, _ or letter " + "</p>")
+document.write("<p>" + "Variable names are case sensitive" + "</p>")
+document.write("<p>" + "Variable names should not be JS keyword" + "</p>")
+
